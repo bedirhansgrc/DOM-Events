c139054abe83a503c23a0f449a6891532d5ad33d
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-12 16:26:26 +0300

    Created Part 5

---
+++ PART-5/app.js
@@ -0,0 +1,31 @@
+//Event Bubbling
+
+// document.querySelector(".container").addEventListener("click",function(){
+//     console.log("Div Container")
+// })
+// document.querySelector(".card.row").addEventListener("click",function(){
+//     console.log("Card Row")
+// })
+// document.querySelectorAll(".card-body")[1].addEventListener("click",function(){
+//     console.log("Card Body")
+// })
+
+//Event Capturing vey Delegation
+
+const cardBody = document.querySelectorAll(".card-body")[1]
+
+
+cardBody.addEventListener("click",run);
+
+function run(e){
+    if (e.target.className === "fa fa-remove" ){
+        console.log("Silme İşlemi")
+    }
+    if (e.target.id === "filter"){
+        console.log("Filtreleme İşlemi")
+    }
+    if (e.target.id === "clear-todos"){
+        console.log("Hepsini Silme İşlemi")
+    }
+    console.log(e.target)
+}
