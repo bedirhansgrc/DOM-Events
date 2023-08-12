b5f67e10695b73e8b5a499dae39f31623305be9e
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-12 15:05:22 +0300

    created Part 3

---
+++ PART-3/app.js
@@ -0,0 +1,31 @@
+const cardBody = document.querySelectorAll(".card-body")[1]
+
+const title = document.querySelector("#tasks-title")
+
+//Click Event
+// title.addEventListener("click",run)
+//Double Click
+// title.addEventListener("dblclick",run)
+//Mouse Down
+// title.addEventListener("mousedown",run)
+//Mouse Up
+//title.addEventListener("mouseup",run)
+//Mouse Over
+//title.addEventListener("mouseover",run)
+//Mouse Out
+//title.addEventListener("mouseout",run)
+
+// cardBody.addEventListener("mouseover",run)
+// cardBody.addEventListener("mouseout",run)
+
+
+//Mouse Enter ve Mouse Leave
+
+cardBody.addEventListener("mouseenter",run)
+cardBody.addEventListener("mouseleave",run) //mouse over ve outtan farkı bodynin içinde birer kez oluyor
+
+
+
+function run(e){
+    console.log(e.type);
+}
