de8cf10d2af57670aeb6ad224275c616bac2e287
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-12 14:41:04 +0300

    created Part 2

---
+++ PART-2/app.js
@@ -0,0 +1,41 @@
+//Klavye Eventleri
+
+//keypress
+// document.addEventListener("keypress", run)
+
+
+// function run(e) {
+//     // console.log(e.which)
+//     console.log(e.key)
+// }
+
+
+//keydown
+// document.addEventListener("keydown", run)
+
+
+// function run(e) {
+//     // console.log(e.which)
+//     console.log(e.key)
+// }
+
+
+//keyup
+
+// document.addEventListener("keyup", run)
+
+
+// function run(e) {
+//     // console.log(e.which)
+//     console.log(e.key)
+// }
+
+const header = document.querySelector(".card-header");
+const todoInput = document.querySelector("#todo");
+
+todoInput.addEventListener("keyup",changeText)
+
+function changeText(e){
+    header.textContent = e.target.value
+    console.log(e.target.value);
+}
